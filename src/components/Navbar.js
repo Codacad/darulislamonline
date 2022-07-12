6a3bee330cc70eb9e2945c9b27c882fe2c311171
Mohd Rizwan <iamrizwan40@gmail.com>
2022-07-12 08:57:05 +0530

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
             <Link className='logo' to='/'><img src={Logo} alt="" /></Link>
             <ul>
                 <li><Link to='/'>Home</Link></li>
-                <li><Link to='#courses'>Courses</Link></li>
+                <li><Link to='/courses'>Courses</Link></li>
                 <li><Link to='/contact'>Contact</Link></li>
                 <li><Link to='/about'>About</Link></li>
             </ul>
