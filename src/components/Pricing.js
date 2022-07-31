125dd276bff3015751e1d6b76f730ccf590cdb6e
Mohd Rizwan <iamrizwan40@gmail.com>
2022-07-31 11:58:46 +0530

    updates

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,45 +1,56 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Done from '../icons/done.svg'
+import {School, Wc, Schedule, Grade, Construction, RepeatOneOn, Groups, MoreTime, MeetingRoom, Shield } from "@mui/icons-material"
 
 const Pricing = () => {
   return (
-    <div className='pricing'>
-        <h1>Choose a Session</h1>
+    <div className='facilites'>
+        <h1>Our Facilities</h1>
         <div className="contents">
-            <div className="one-to-one">
-                <div className="card">
-                    <div className="header">
-                        <h3>One-to-One Session</h3>
-                    </div>
-                    <div className="body">
-                        <ul>
-                            <li><img src={Done} alt="" /><span>Lorem ipsum dolor sit amet.</span></li>
-                            <li><img src={Done} alt="" /><span>Lorem ipsum dolor sit amet.</span></li>
-                            <li><img src={Done} alt="" /><span>Lorem ipsum dolor sit amet.</span></li>
-                            <li><img src={Done} alt="" /><span>Lorem ipsum dolor sit amet.</span></li>
-                            <li><img src={Done} alt="" /><span>Lorem ipsum dolor sit amet.</span></li>
-                        </ul>
-                        <Link to="#">Next</Link>
-                    </div>
-                </div>
+            <div className="facility">
+                <School></School>
+                <span>Qualified Teachers</span>
             </div>
-            <div className="group">
-                <div className="card">
-                    <div className="header">
-                        <h3>Group Session</h3>
-                    </div>
-                    <div className="body">
-                        <ul>
-                            <li><img src={Done} alt="" /><span>Lorem ipsum dolor sit amet.</span></li>
-                            <li><img src={Done} alt="" /><span>Lorem ipsum dolor sit amet.</span></li>
-                            <li><img src={Done} alt="" /><span>Lorem ipsum dolor sit amet.</span></li>
-                            <li><img src={Done} alt="" /><span>Lorem ipsum dolor sit amet.</span></li>
-                            <li><img src={Done} alt="" /><span>Lorem ipsum dolor sit amet.</span></li>
-                        </ul>
-                        <Link to="#">Next</Link>
-                    </div>
-                </div>
+            <div className="facility">
+                <Schedule></Schedule>
+                <span>Classes Based on Flexible Schedule</span>
+            </div>
+            <div className="facility">
+                <RepeatOneOn></RepeatOneOn>
+               <span>One on One Sessions</span>
+            </div>
+            <div className="facility">
+                <Wc></Wc>
+                <span>Male and Female Teachers</span>
+            </div>
+            <div className="facility">
+                <Schedule></Schedule>
+                <span>Classes Based on Flexible Schedule</span>
+            </div>
+            <div className="facility">
+                <Grade></Grade>
+                <span>Special Classes</span>
+            </div>
+            <div className="facility">
+                <Construction></Construction>
+                <span>Demo Classes</span>
+            </div>
+            <div className="facility">
+                <Shield></Shield>
+                <span>Qualitiy Education</span>
+            </div>
+            <div className="facility">
+                <MeetingRoom></MeetingRoom>
+                <span>Parents' Meeting</span>
+            </div>
+            <div className="facility">
+                <MoreTime></MoreTime>
+                <span>Extra Time</span>
+            </div>
+            <div className="facility">
+                <MeetingRoom></MeetingRoom>
+                <span>Students' Meeting on each Sunday</span>
             </div>
         </div>
     </div>
